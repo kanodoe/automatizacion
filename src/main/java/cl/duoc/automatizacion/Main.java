343695fcc4d0e9fa5b0e2c0456ecf905d5e898cc
Alejandro Cornejo <acornejs@everis.com>
2019-08-14 20:01:21 -0400

    first commit

---
+++ src/main/java/cl/duoc/automatizacion/Main.java
@@ -0,0 +1,30 @@
+package cl.duoc.automatizacion;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.navigate().to(new URL("https://www.google.com"));
+
+        WebDriverWait wait = new WebDriverWait(webDriver, 10000);
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.tagName("input"), 0));
+
+        WebElement input = webDriver.findElement(By.cssSelector("#lst-ib"));
+
+        input.sendKeys("Spark Java file upload example - First Few Lines");
+        input.sendKeys(Keys.ENTER);
+    }
+
+}
